200c4ba35a544368cf57c8a4b0864a0f72d83ba7
Warden <whiteraven1000@gmail.com>
2021-11-29 21:41:06 +0100

    [2911]-[Script] Puzle 3 completo
    El código que soporta al puzle 3 está hecho y probado. Se pasa al cuarto puzle.

--- Assets/Scripts/Sala1/LadoCubo.cs
+++ Assets/Scripts/Sala1/LadoCubo.cs
@@ -4,23 +4,78 @@ using UnityEngine;
 
 public class LadoCubo : MonoBehaviour
 {
-    public int indice;
-    MeshRenderer meshCara;
+    [SerializeField]
+    int indice;
+    SpriteRenderer meshCara;
     List<Material> materialesColores;
-    CuboRubik cubo;
+    Puzle3 cubo;
 
     private void Start()
     {
-        meshCara = gameObject.GetComponent<MeshRenderer>(); //habrá 6 colores aleatorios: rojo, azul, amarillo, verde, blanco y morado. Irán numerados de 0 a 5
-        materialesColores = FindObjectOfType<CuboRubik>().GetColoresMateriales();
-        cubo = FindObjectOfType<CuboRubik>();
+        meshCara = gameObject.GetComponent<SpriteRenderer>(); //habrá 6 colores aleatorios: rojo, azul, amarillo, verde, blanco y morado. Irán numerados de 0 a 5
+        materialesColores = FindObjectOfType<Puzle3>().GetColoresMateriales();
+        cubo = FindObjectOfType<Puzle3>();
     }
 
     public void CambiarColorLado() //cambia el color de esa cara con un clic
     {
-        int numero = cubo.GetEstadoCubo()[indice];
+        if (!cubo.ComprobarSiEstaResuelto())
+        {
+            int numero = cubo.GetEstadoCubo()[indice];
 
-        if (numero < 5)
+            /*if (numero < 5)
+            {
+                numero++;
+            }
+            else
+            {
+                numero = 0;
+            }
+            //meshCara.material = materialesColores[numero];
+            cubo.SetEstadoCubo(numero, indice);
+            */
+            switch (numero)
+            {
+                case 0: //rojo
+                    meshCara.color = new Color(0, 1, 1, 1);
+                    numero++;
+                    break;
+                case 1: //azul
+                    meshCara.color = new Color(1, 1, 0, 1);
+                    numero++;
+                    break;
+                case 2: //amarillo
+                    meshCara.color = new Color(0, 1, 0, 1);
+                    numero++;
+                    break;
+                case 3: //verde
+                    meshCara.color = new Color(1, 1, 1, 1);
+                    numero++;
+                    break;
+                case 4: // blanco
+                    meshCara.color = new Color(1, 0, 1, 1);
+                    numero++;
+                    break;
+                case 5: //morado
+                    meshCara.color = new Color(1, 0, 0, 1);
+                    numero = 0;
+                    break;
+            }
+            cubo.SetEstadoCubo(numero, indice);
+
+        }
+        else
+        {
+            Debug.Log("Ya está resuelto.");
+        }
+
+
+    }
+
+    public void CambiarColorLadoInicio(int numero) //cambia el color de esa cara con un clic
+    {
+
+        /*if (numero < 5)
         {
             numero++;
         }
@@ -28,8 +83,61 @@ public class LadoCubo : MonoBehaviour
         {
             numero = 0;
         }
-        meshCara.material = materialesColores[numero];
+        //meshCara.material = materialesColores[numero];
+        cubo.SetEstadoCubo(numero, indice);
+        */
+
+        if (meshCara == null)
+        {
+            Debug.Log("Soy nulo y voy a agregarme");
+            meshCara = GetComponent<SpriteRenderer>();
+            Debug.Log("Ahora meshCara es: " + meshCara);
+        }
+
+        switch (numero)
+        {
+            case 0: //rojo
+                meshCara.color = new Color(1, 0, 0, 1);
+                break;
+            case 1: //azul
+                meshCara.color = new Color(0, 1, 1, 1);
+                break;
+            case 2: //amarillo
+                meshCara.color = new Color(1, 1, 0, 1);
+                break;
+            case 3: //verde
+                meshCara.color = new Color(0, 1, 0, 1);
+                break;
+            case 4: // blanco
+                meshCara.color = new Color(1, 1, 1, 1);
+                break;
+            case 5: //morado
+                meshCara.color = new Color(1, 0, 1, 1);
+                break;
+        }
+
 
     }
 
+    public int GetIndice()
+    {
+        return indice;
+    }
+
+    public void SetIndice(int indi)
+    {
+        indice = indi;
+    }
+
+    private void OnMouseDown()
+    {
+        CambiarColorLado();
+    }
+
+    private void OnMouseOver()
+    {
+        Debug.Log(gameObject.name + ", " + gameObject.transform.parent.name + ", " + indice);
+    }
+
 }
+
--- Assets/Scripts/Sala1/Puzle3.cs
+++ Assets/Scripts/Sala1/Puzle3.cs
@@ -1,19 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Puzle3 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public CuboRubik cubo;
-    void Start()
+
+    [SerializeField]
+    List<int> ladosCubo = new List<int>();
+    [SerializeField]
+    List<int> solucionCubo = new List<int>();
+    public List<LadoCubo> ladosFisicos;
+    public List<Material> materialesColores;
+
+    void Start() //habrá 6 colores aleatorios: rojo, azul, amarillo, verde, blanco y morado. Irán numerados de 0 a 5
     {
-        
+        ladosFisicos = FindObjectsOfType<LadoCubo>().ToList();
+
+        for (int i = 0; i < ladosFisicos.Count; i++)
+        {
+
+            int colorInicial = Random.Range(0, 6);
+            ladosCubo.Add(colorInicial);
+            solucionCubo.Add(Random.Range(0, 6));
+            ladosFisicos[i].SetIndice(i);
+
+            ladosFisicos[i].gameObject.AddComponent<BoxCollider>();
+        }
+        int j = 0;
+        foreach(LadoCubo lad in ladosFisicos)
+        {
+            Debug.Log(j);
+            j++;
+            lad.CambiarColorLadoInicio(ladosCubo[lad.GetIndice()]);
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CerrarPuzle()
     {
-        
+
     }
+
+    public bool ComprobarSiEstaResuelto()
+    {
+        bool estaResuelto = true;
+
+        for(int i = 0; i < ladosCubo.Count; i++)
+        {
+            if(ladosCubo[i] != solucionCubo[i])
+            {
+                estaResuelto = false;
+                break;
+            }
+        }
+
+        return estaResuelto;
+    }
+
+    public List<int> GetEstadoCubo()
+    {
+        return ladosCubo;
+    }
+
+    public void SetEstadoCubo(int numero, int indice)
+    {
+        ladosCubo[indice] = numero;
+    }
+
+    public List<Material> GetColoresMateriales()
+    {
+        return materialesColores;
+    }
+
 }
